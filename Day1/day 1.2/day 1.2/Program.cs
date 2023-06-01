5dc3f8bed6b61b8b0bcf433e8d2dc780f0c8ad0c
sheroz-18 <sherozpulatov5@gmail.com>
2023-06-01 19:38:53 +0500

    remove file

---
+++ Day1/day 1.2/day 1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace QuadraticEquationSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для решения квадратных уравнений ax^2+ bx + c = 0");
+            Console.WriteLine("Введите a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c:");
+            double c = double.Parse(Console.ReadLine());
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                Console.WriteLine("Уравнение не имеет действительных корней.");
+            }
+            else if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("Уравнение имеет один корень: x = " + x);
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                Console.WriteLine("Уравнение имеет два корня: x1 = " + x1 + ", x2 = " + x2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
