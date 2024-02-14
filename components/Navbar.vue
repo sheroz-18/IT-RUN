e8573d52374e86b64cc2ff6038fe18536411e69e
Sheroz <123716684+sheroz-18@users.noreply.github.com>
2024-02-14 15:38:39 +0300

    'doasdad'

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-[10px] desktop:text-[50px]">hello world</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'mobile': '640px',
+        'desktop': '1140px',
+      },
+    },
+  },
+  plugins: [],
+}
+
